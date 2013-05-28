1cafb8dc62a0ba4bc9fdf5911e0eaa58fdd5c6ad
Alanah <Lana383@yahoo.com>
2013-05-28 17:06:41 -0400

    delete option updated

--- todo_app.rb
+++ todo_app.rb
@@ -4,43 +4,93 @@
 	def add_task(addtask)
 		#todo << addtask.push
 		@todo.push(addtask)
+		#would like to assign a # to each task
 	end	
 
 	def remove_task(deletetask)
-
+		#@todo.pop(deletetask)
+		#@todo.select {|d| d.match(/^deletetask/)}
+		#@todo.select {|d| d.include? 'deletetask' }
+		@todo.delete('deletetask')
 	end	
 
 	def complete_task(completed)
 
 	end		
 
-	puts "1. Add Task"
-	puts "2. Remove Task"
-	puts "3. Mark Task Complete" 
-	puts "4. View Todo List"
+option_selected = "n"	
+
+while option_selected == "n"  do
+
+	puts "A: Add Task"
+	puts "D: Delete Task"
+	puts "C: Mark Task Complete (pending)"
+	puts "V: View Todo List"
+	puts "E: Exit"
 	puts ""
 	puts "Choose an option:"
-	
-	option = gets.chomp
 
-	if option == "1"
-		puts "Name the task you wish to add:"
-		add = gets.chomp
-		add_task(add)
+	option = gets.chomp.upcase
 
-	elsif option == "2" 
+
+	if option == "A"		
+		puts "Name the task you wish to add: (type 'done' when finished)"
+		add = gets.chomp.downcase
+
+		if add != 'done'
+			add_task(add)
+			puts ""
+			puts "******TODO LIST******"
+			puts @todo	
+			puts "*********************"
+			puts ""
+			option_selected = "n"
+		else
+			puts "test"	
+		end	
+
+	elsif option == "D"
 		puts "Name the task you wish to delete"
 		remove = gets.chomp
-		remove_task(deletetask)
-	elsif option == "3"
-		puts "Name the task you wish to mark complete"
-	elsif option == "4"
-	puts "******TODO LIST******"
-		@todo[]	
-	else
-		puts "That option is not available."		
-	end	
-		 		 
-puts "******TODO LIST******"
+			if remove != nil
+				puts "Are you sure you want to delete '#{remove}' from the list? (Y/N)"
+				task_removal = gets.chomp.upcase
+					if task_removal == "Y" 
+						remove_task(remove)
+						puts "'#{remove}' has been deleted"				
+					end	
 
-puts @todo
+			else
+				puts "That is not an option."
+			end	
+			
+
+	elsif option == "C"
+		puts "Sorry, this option is under construction"
+		
+		#puts "Name the task you wish to mark complete"
+
+	elsif option == "V"		
+		puts ""
+		puts "******TODO LIST******"
+		puts @todo
+		puts "*********************"
+		puts ""
+		puts ""
+		option_selected = "n"
+
+
+	elsif option == "E"
+		puts "You have exited the program."
+		option_selected = "y"
+
+	else
+		puts "That option is not available."	
+	end	
+
+
+end	
+		 		 
+#puts "******TODO LIST******"
+
+#puts @todo
