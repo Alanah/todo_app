8ce784c97e6add34866e886e30fcb564904c24d0
Alanah <Lana383@yahoo.com>
2013-06-20 08:22:38 -0400

    Turning native ruby to a rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoApp::Application.config.secret_token = '3e0f5c0451192c97f99fc6417543172838be835c1c37b684da09727f834c59ab45a4af03620f9e57b3adace0fd30163982d7b566fa12d847a12a31da9d3cdbb2'
